6591c21c48050ba3d654be9778cd9e601d30bbca
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-02-14 12:57:42 +0200

    adding all filters

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,47 +1,85 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export const Filter = (products) => {
-    const sizesArr = []
-    const colorsArr = []
-    const categoriesArr = [] 
-    products.map((product) => {
-        product.sizes.map((size) => {
-            if (!sizesArr.includes(size)) { sizesArr.push(size)}})
-        product.categories.map((category) => {
-            if (!categoriesArr.includes(category)) {categoriesArr.push(category)}})
-        if (!colorsArr.includes(product.colors)) {colorsArr.push(product.colors)}})
-    return {sizesArr,colorsArr,categoriesArr}
-}
+  let sizesArr = [];
+  let colorsArr = [];
+  let categoriesArr = [];
+  products.map((product) => {
+    product.sizes.map((size) => {
+      if (!sizesArr.includes(size)) {
+        sizesArr.push(size);
+      }
+    });
+    product.categories.map((category) => {
+      if (!categoriesArr.includes(category)) {
+        categoriesArr.push(category);
+      }
+    });
+    if (!colorsArr.includes(product.colors)) {
+      colorsArr.push(product.colors);
+    }
+  });
+  return { sizesArr, colorsArr, categoriesArr };
+};
 
 export default function ProductCard({ products }) {
-    const { sizesArr, colorsArr, categoriesArr } = Filter(products)
-    const filters = [categoriesArr, sizesArr, colorsArr]
+  const { sizesArr, colorsArr, categoriesArr } = Filter(products);
+  const filters = [
+    { type: "categories", values: categoriesArr },
+    { type: "sizes", values: sizesArr },
+    { type: "colors", values: colorsArr },
+  ];
 
-    const [selectedSizes, setSelectedSizes] = useState([]);
-    const handleSizeChange = (event) => {
-        const size = event.target.name;
-        if (event.target.checked) {
-            setSelectedSizes((prevSelectedSizes) => [...prevSelectedSizes, size]);
-        } else {
-            setSelectedSizes((prevSelectedSizes) => prevSelectedSizes.filter((selectedSize) => selectedSize !== size));
-        }
-    };
+  const [selectedFilters, setSelectedFilters] = useState({
+    categories: [],
+    sizes: [],
+    colors: [],
+  });
 
-    return (
+  const handleFilterChange = (event) => {
+    const filterType = event.target.getAttribute("data-filter-type");
+    const filterValue = event.target.name;
+
+    setSelectedFilters((prevSelectedFilters) => ({
+      ...prevSelectedFilters,
+      [filterType]: event.target.checked
+        ? [...prevSelectedFilters[filterType], filterValue]
+        : prevSelectedFilters[filterType].filter(
+            (selectedValue) => selectedValue !== filterValue
+          ),
+    }));
+  };
+
+  return (
     <>
-    <button>FILTER</button>
-        {sizesArr.map((size) => (
-            <div key={size}>
-                <label htmlFor={size}>{size}</label>
-                <input type="checkbox" name={size} id={size} onChange={handleSizeChange}/>
-                </div>
-        ))}
-    {products.map((product) => {
-        const hasSelectedSize = selectedSizes.length === 0 || selectedSizes.some((size) =>
-            product.sizes.includes(size));
+      <button>FILTER</button>
+      {filters.map((filter) =>
+        filter.values.map((value) => (
+          <div key={value}>
+            <label htmlFor={value}>{value}</label>
+            <input
+              type="checkbox"
+              name={value}
+              id={value}
+              data-filter-type={filter.type}
+              onChange={handleFilterChange}
+            />
+          </div>
+        ))
+      )}
+      {products.map((product) => {
+        const hasSelectedFilters = (
+          (selectedFilters.categories.length === 0 || selectedFilters.categories.every((category) => product.categories.includes(category))) &&
+          (selectedFilters.sizes.length === 0 || selectedFilters.sizes.every((size) => product.sizes.includes(size))) &&
+          (selectedFilters.colors.length === 0 || selectedFilters.colors.includes(product.colors))
+        );
         return (
-            <Link to={`/product/${product.slug}`} key={product.slug} className={!hasSelectedSize ? "hidden" : null}>
+          <Link
+            to={`/product/${product.slug}`}
+            key={product.slug}
+            className={!hasSelectedFilters ? "hidden" : null}
+          >
                 <div className="max-w-lg  relative">
                     <p>{product.name}</p>
                     <div>
