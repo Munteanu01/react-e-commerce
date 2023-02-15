f124bc3256b3ca3e16013ec8bfc9c280a78e30c0
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-02-15 14:43:42 +0200

    adding sorting

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -8,6 +8,8 @@ export default function ProductCard({ products }) {
     sizes: [],
     colors: [],
   });
+  const [selectedSort, setSelectedSort] = useState("price");
+  
   const handleFilterChange = (event) => {
     const filterType = event.target.getAttribute("data-filter-type");
     const filterValue = event.target.name;
@@ -20,15 +22,37 @@ export default function ProductCard({ products }) {
           ),
     }));
   };
+  
+  const handleSortChange = (event) => {
+    setSelectedSort(event.target.value);
+  };
+
+  const sortedProducts = [...products].sort((a, b) => {
+    if (selectedSort === "name") {
+      return a.name.localeCompare(b.name);
+    } else {
+      const ascending = selectedSort === "price";
+      const comparison = a.price - b.price;
+      return ascending ? comparison : -comparison;
+    }
+  });
 
   return (
     <>
+      <div>
+        <label htmlFor="sort-select">Sort by:</label>
+        <select className="text-black" id="sort-select" value={selectedSort} onChange={handleSortChange}>
+        <option value="price">Price (low to high)</option>
+  <option value="-price">Price (high to low)</option>
+          <option value="name">Name</option>
+        </select>
+      </div>
       <Filter
         products={products}
         handleFilterChange={handleFilterChange}
         selectedFilters={selectedFilters}
       />
-      {products.map((product) => {
+      {sortedProducts.map((product) => {
         const hasSelectedFilters = (
           (selectedFilters.categories.length === 0 || selectedFilters.categories.some((category) => product.categories.includes(category))) &&
           (selectedFilters.sizes.length === 0 || selectedFilters.sizes.some((size) => product.sizes.includes(size))) &&
@@ -40,17 +64,17 @@ export default function ProductCard({ products }) {
             key={product.slug}
             className={!hasSelectedFilters ? "hidden" : null}
           >
-                <div className="max-w-lg  relative">
-                    <p>{product.name}</p>
-                    <div>
-                        {product.new && (<div className="bg-black text-white p-3 m-2 absolute right-0">NEW</div>)}
-                        <img src={product.image.url} alt="" />
-                    </div>
-                    <p>{product.price}</p>
-                </div>
-            </Link>
+            <div className="max-w-lg  relative">
+              <p>{product.name}</p>
+              <div>
+                {product.new && (<div className="bg-black text-white p-3 m-2 absolute right-0">NEW</div>)}
+                <img src={product.image.url} alt="" />
+              </div>
+              <p>{product.price}</p>
+            </div>
+          </Link>
         );
-    })}
+      })}
     </>
-    );
+  );
 }
