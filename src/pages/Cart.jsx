85ca45cee63f11a6d693d7940fd9a826117436ad
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-02-17 21:10:23 +0200

    cart add remove

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 export default function Cart() {
-    const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    const [cartItems, setCartItems] = useState(() => {
+      const storedCartItems = localStorage.getItem("cartItems");
+      return storedCartItems ? JSON.parse(storedCartItems) : [];
+    });
+  
+    const handleIncreaseQuantity = (id) => {
+      const updatedItems = cartItems.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+      setCartItems(updatedItems);
+      localStorage.setItem("cartItems", JSON.stringify(updatedItems));
+    };
+  
+    const handleDecreaseQuantity = (id) => {
+      const updatedItems = cartItems
+        .map((item) =>
+          item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+        )
+        .filter((item) => item.quantity > 0);
+      setCartItems(updatedItems);
+      localStorage.setItem("cartItems", JSON.stringify(updatedItems));
+    };
+  
+    const handleRemoveItem = (id) => {
+      const updatedItems = cartItems.filter((item) => item.id !== id);
+      setCartItems(updatedItems);
+      localStorage.setItem("cartItems", JSON.stringify(updatedItems));
+    };
   
     return (
       <div className="pt-20">
@@ -7,7 +36,11 @@ export default function Cart() {
         <ul>
           {cartItems.map((item) => (
             <li key={item.id}>
-              {item.name} - {item.quantity}
+              {item.name} 
+              <button className="mx-3" onClick={() => handleDecreaseQuantity(item.id)}>-</button>
+              {item.quantity}
+              <button className="mx-3" onClick={() => handleIncreaseQuantity(item.id)}>+</button>
+              <button className="mx-3" onClick={() => handleRemoveItem(item.id)}>Remove</button>
             </li>
           ))}
         </ul>
