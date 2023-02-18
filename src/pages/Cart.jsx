35bab0edb4e5e62809f966e406379eea1f6cadea
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-02-18 08:54:30 +0200

    fixing

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,49 +1,55 @@
 import { useState } from "react";
 
 export default function Cart() {
-    const [cartItems, setCartItems] = useState(() => {
-      const storedCartItems = localStorage.getItem("cartItems");
-      return storedCartItems ? JSON.parse(storedCartItems) : [];
-    });
+  const [cartItems, setCartItems] = useState(() => {
+    const storedCartItems = localStorage.getItem("cartItems");
+    return storedCartItems ? JSON.parse(storedCartItems) : [];
+  });
   
-    const handleIncreaseQuantity = (id) => {
-      const updatedItems = cartItems.map((item) =>
-        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
-      );
-      setCartItems(updatedItems);
-      localStorage.setItem("cartItems", JSON.stringify(updatedItems));
-    };
-  
-    const handleDecreaseQuantity = (id) => {
-      const updatedItems = cartItems
-        .map((item) =>
-          item.id === id ? { ...item, quantity: item.quantity - 1 } : item
-        )
-        .filter((item) => item.quantity > 0);
-      setCartItems(updatedItems);
-      localStorage.setItem("cartItems", JSON.stringify(updatedItems));
-    };
-  
-    const handleRemoveItem = (id) => {
-      const updatedItems = cartItems.filter((item) => item.id !== id);
-      setCartItems(updatedItems);
-      localStorage.setItem("cartItems", JSON.stringify(updatedItems));
-    };
-  
-    return (
-      <div className="pt-20">
-        <h1>Cart</h1>
-        <ul>
-          {cartItems.map((item) => (
-            <li key={item.id}>
-              {item.name} 
-              <button className="mx-3" onClick={() => handleDecreaseQuantity(item.id)}>-</button>
-              {item.quantity}
-              <button className="mx-3" onClick={() => handleIncreaseQuantity(item.id)}>+</button>
-              <button className="mx-3" onClick={() => handleRemoveItem(item.id)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-      </div>
+  const handleIncreaseQuantity = (id, size) => {
+    const updatedItems = cartItems.map((item) =>
+      item.id === id && item.size === size
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
     );
-  }
+    setCartItems(updatedItems);
+    localStorage.setItem("cartItems", JSON.stringify(updatedItems));
+  };
+  
+  const handleDecreaseQuantity = (id, size) => {
+    const updatedItems = cartItems
+      .map((item) =>
+        item.id === id && item.size === size
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+      .filter((item) => item.quantity > 0);
+    setCartItems(updatedItems);
+    localStorage.setItem("cartItems", JSON.stringify(updatedItems));
+  };
+  
+  const handleRemoveItem = (id, size) => {
+    const updatedItems = cartItems.filter((item) => item.id !== id || item.size !== size);
+    setCartItems(updatedItems);
+    localStorage.setItem("cartItems", JSON.stringify(updatedItems));
+  };
+  
+  return (
+    <div className="pt-20">
+      <h1>Cart</h1>
+      <ul>
+        {cartItems.map((item) => (
+          <li key={`${item.id}-${item.size}`}>
+            <p>{item.name} {item.size}</p>
+  
+            <button className="mx-3" onClick={() => handleDecreaseQuantity(item.id, item.size)}>-</button>
+            {item.quantity}
+            <button className="mx-3" onClick={() => handleIncreaseQuantity(item.id, item.size)}>+</button>
+
+            <button className="mx-3" onClick={() => handleRemoveItem(item.id, item.size)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,34 +4,49 @@ import { useParams } from "react-router-dom";
 export default function Product({ products }) {
   const params = useParams();
   const product = products.find((p) => p.slug === params.slug);
-
+  const [selectedSize, setSelectedSize] = useState("");
   const [cartItems, setCartItems] = useState(() => {
     const storedCartItems = localStorage.getItem("cartItems");
     return storedCartItems ? JSON.parse(storedCartItems) : [];
   });
 
   const handleAddToCart = () => {
-    const index = cartItems.findIndex((item) => item.id === product.id);
-    if (index === -1) {
-      // Product is not in the cart, add it with quantity 1
-      setCartItems([...cartItems, { ...product, quantity: 1 }]);
-    } else {
-      // Product is already in the cart, increment its quantity
-      const updatedItems = [...cartItems];
-      updatedItems[index].quantity++;
-      setCartItems(updatedItems);
+    if (selectedSize) {
+      const index = cartItems.findIndex((item) => item.id === product.id && item.size === selectedSize);
+      if (index === -1) {
+        setCartItems([...cartItems, { ...product, size: selectedSize, quantity: 1 }]);
+      } else {
+        const updatedItems = [...cartItems];
+        updatedItems[index].quantity++;
+        setCartItems(updatedItems);
+      }
     }
   };
-
+ 
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }, [cartItems]);
 
   return (
     <div className="pt-40 flex">
-      <img className="w-[200px]" src={product?.image.url} alt="" />
-      <h1>{product?.name}</h1>
-      <button onClick={handleAddToCart}>Add</button>
-    </div>
+    <img className="w-[200px]" src={product?.image.url} alt="" />
+    <h1>{product?.name}</h1>
+    {product?.sizes && (
+      <div>
+        <label htmlFor="size">Size:</label>
+        <select id="size" value={selectedSize} onChange={(e) => setSelectedSize(e.target.value)}>
+          <option value="">Select a size</option>
+          {product.sizes.map((size) => (
+            <option key={size} value={size}>
+              {size}
+            </option>
+          ))}
+        </select>
+      </div>
+    )}
+    <button onClick={handleAddToCart} disabled={!selectedSize}>
+      Add
+    </button>
+  </div>
   );
 }
