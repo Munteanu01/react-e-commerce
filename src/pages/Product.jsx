356aaf2f2a2cbb4c20b232ff6f957749806e6af4
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-02-18 14:36:04 +0200

    rewriting code for unique products

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,7 +4,9 @@ import { useParams } from "react-router-dom";
 export default function Product({ products }) {
   const params = useParams();
   const product = products.find((p) => p.slug === params.slug);
-  const [selectedSize, setSelectedSize] = useState("");
+  const [selectedSize, setSelectedSize] = useState(
+    product.sizes.includes("UNIQUE") ? "UNIQUE" : ""
+  );
   const [cartItems, setCartItems] = useState(() => {
     const storedCartItems = localStorage.getItem("cartItems");
     return storedCartItems ? JSON.parse(storedCartItems) : [];
@@ -27,26 +29,26 @@ export default function Product({ products }) {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }, [cartItems]);
 
-  return (
-    <div className="pt-40 flex">
-    <img className="w-[200px]" src={product?.image.url} alt="" />
-    <h1>{product?.name}</h1>
-    {product?.sizes && (
-      <div>
-        <label htmlFor="size">Size:</label>
-        <select id="size" value={selectedSize} onChange={(e) => setSelectedSize(e.target.value)}>
-          <option value="">Select a size</option>
-          {product.sizes.map((size) => (
-            <option key={size} value={size}>
-              {size}
-            </option>
-          ))}
-        </select>
-      </div>
-    )}
-    <button onClick={handleAddToCart} disabled={!selectedSize}>
-      Add
-    </button>
-  </div>
-  );
+  return  (
+    <>
+      {product ? (
+        <div className="pt-40 flex">
+          <img className="w-[200px]" src={product.image.url} alt="" />
+          <h1>{product.name}</h1>
+          {!product.sizes.includes("UNIQUE") && (
+           <div>
+             <label htmlFor="size">Size:</label>
+             <select id="size" value={selectedSize} onChange={(e) => setSelectedSize(e.target.value)}>
+               <option value="">Select a size</option>
+               {product.sizes.map((size) => (
+                     <option key={size} value={size}>{size}</option>
+                ))}
+             </select>
+           </div>
+          )}
+          <button onClick={handleAddToCart} disabled={!selectedSize}>Add</button>
+        </div>
+      ) : (<h1>Not found</h1>)}
+    </>
+    )
 }
