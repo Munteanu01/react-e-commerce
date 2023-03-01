3991c61b81bcd65ba9d9b8273cebd2b3fa0d8109
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-03-01 10:06:07 +0200

    creating the form layout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,155 +1,103 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function CheckoutForm() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [address, setAddress] = useState('');
-    const [city, setCity] = useState('');
-    const [state, setState] = useState('');
-    const [zip, setZip] = useState('');
-    const [cardNumber, setCardNumber] = useState('');
-    const [expMonth, setExpMonth] = useState('');
-    const [expYear, setExpYear] = useState('');
-    const [cvv, setCvv] = useState('');
-    const handleSubmit = (e) => {
-      e.preventDefault();
-    };
-  
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [furtherInformation, setFurtherInformation] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [town, setTown] = useState("");
+  const [district, setDistrict] = useState("");
+  const [country, setCountry] = useState("");
+
+  const [deliverySection, setDeliverySection] = useState(true);
+  const [paymentSection, setPaymentSection] = useState(false);
+  const [showThankYou, setShowThankYou] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.clear();
+    setShowThankYou(true);
+  };
+  if(showThankYou){
+    return(
+      <div className="h-screen pt-20">
+          <p className="text-4xl">THANKS</p>
+          <Link to={'/'}>
+            <button>GO TO MAIN PAGE</button>
+          </Link>
+      </div>
+    )
+  }
+  else{
     return (
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto  p-4  rounded-lg">
-        <h2 className="text-2xl font-bold mb-4 mt-20">CHECKOUT</h2>
-        <div className="mb-4">
-          <label htmlFor="name" className="block font-bold mb-2" required>
-            NAME
-          </label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full border py-2 px-3 rounded"
-            required
-          />
+     <div className="pt-24">
+        <div className="flex justify-center font-bold">
+        <div className="flex">
+            <p className="rounded-full bg-black text-white border-2 border-transparent mr-3  px-[9px] pt-[5px] pb-[3px] text-xs font-extrabold">1</p>
+            <button className="pt-1" onClick={() => {setPaymentSection(false); setDeliverySection(true)}}>DELIVERY</button>
+          </div>
+          
+          <div className={`w-20 border-b-2 flex my-auto mx-5 ${paymentSection ? 'border-black' : 'border-[#949494]'}`}/>
+
+          <div className={`flex  ${paymentSection ? 'text-black' :  'text-[#949494]' }`}>
+            <p className={`rounded-full  border-2 border-[#949494] mr-3  px-[9px] pt-[5px] pb-[3px] text-xs font-extrabold 
+               ${paymentSection ? 'border-transparent bg-black text-white'  : 'border-[#949494]'}`}>2</p>
+            <button className="pt-1" onClick={() => {setPaymentSection(true); setDeliverySection(false)}}>PAYMENT</button>
+          </div>
         </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block font-bold mb-2">
-            EMAIL
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full border py-2 px-3 rounded"
-            required
-          />
+        {deliverySection && (
+          <div>
+          <h1>DELIVERY</h1>
+          <form onSubmit={() => {setPaymentSection(true); setDeliverySection(false)}}>
+            <label htmlFor="firstName">First Name:</label>
+            <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            <br />
+            <label htmlFor="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            <br />
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <br />
+            <label htmlFor="mobileNumber">Mobile Number:</label>
+            <input type="tel" id="mobileNumber" name="mobileNumber" value={mobileNumber} onChange={(e) => setMobileNumber(e.target.value)} />
+            <br />
+            <label htmlFor="address">Address:</label>
+            <input type="text" id="address" name="address" value={address} onChange={(e) => setAddress(e.target.value)} />
+            <br />
+            <label htmlFor="furtherInformation">Further Information:</label>
+            <input type="text" id="furtherInformation" name="furtherInformation" value={furtherInformation} onChange={(e) => setFurtherInformation(e.target.value)} />
+            <br />
+            <label htmlFor="postalCode">ZIP/Postal Code:</label>
+            <input type="text" id="postalCode" name="postalCode" value={postalCode} onChange={(e) => setPostalCode(e.target.value)} />
+            <br />
+            <label htmlFor="town">Town:</label>
+            <input type="text" id="town" name="town" value={town} onChange={(e) => setTown(e.target.value)} />
+            <br />
+            <label htmlFor="district">District:</label>
+            <input type="text" id="district" name="district" value={district} onChange={(e) => setDistrict(e.target.value)} />
+            <br />
+            <label htmlFor="country">Country:</label>
+            <select id="country" name="country" value={country} onChange={(e) => setCountry(e.target.value)}>
+              <option value="">Select Country</option>
+              <option value="Romania">Romania</option>
+              <option value="United Kingdom">United Kingdom</option>
+              <option value="Germany">Germany</option>
+            </select>
+            <br />
+            <button type="submit">Continue to Payment</button>
+          </form>
         </div>
-        <div className="mb-4">
-          <label htmlFor="address" className="block font-bold mb-2">
-            ADDRESS
-          </label>
-          <input
-            type="text"
-            id="address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            className="w-full border py-2 px-3 rounded"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="city" className="block font-bold mb-2">
-            CITY
-          </label>
-          <input
-            type="text"
-            id="city"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            className="w-full border py-2 px-3 rounded"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="state" className="block font-bold mb-2">
-            STATE
-          </label>
-          <input
-            type="text"
-            id="state"
-            value={state}
-            onChange={(e) => setState(e.target.value)}
-            className="w-full border py-2 px-3 rounded"
-            required
-          />
-        </div>
-        <div className="mb-4">
-            <label htmlFor="zip" className="block font-bold mb-2">
-            ZIP
-            </label>
-            <input
-            type="text"
-            id="zip"
-            value={zip}
-            onChange={(e) => setZip(e.target.value)}
-            className="w-full border py-2 px-3 rounded"
-            required/>
-        </div>
-        <hr className="my-8" />
-        <div className="mb-4">
-            <label htmlFor="cardNumber" className="block font-bold mb-2">
-            CARD NUMBER
-            </label>
-            <input
-            type="text"
-            id="cardNumber"
-            value={cardNumber}
-            onChange={(e) => setCardNumber(e.target.value)}
-            className="w-full border py-2 px-3 rounded"
-            required/>
-        </div>
-        <div className="flex mb-4">
-            <div className="w-1/2 mr-4">
-            <label htmlFor="expMonth" className="block font-bold mb-2">
-                EXPIRATION MONTH
-            </label>
-            <input
-                type="text"
-                id="expMonth"
-                value={expMonth}
-                onChange={(e) => setExpMonth(e.target.value)}
-                className="w-full border py-2 px-3 rounded"
-                required/>
-            </div>
-            <div className="w-1/2 ml-4">
-            <label htmlFor="expYear" className="block font-bold mb-2">
-                EXPIRATION YEAR
-            </label>
-            <input
-                type="text"
-                id="expYear"
-                value={expYear}
-                onChange={(e) => setExpYear(e.target.value)}
-                className="w-full border py-2 px-3 rounded"
-                required/>
-            </div>
-        </div>
-        <div className="mb-4">
-            <label htmlFor="cvv" className="block font-bold mb-2">
-            CVV
-            </label>
-            <input
-            type="text"
-            id="cvv"
-            value={cvv}
-            onChange={(e) => setCvv(e.target.value)}
-            className="w-full border py-2 px-3 rounded"
-            required
-            />
-        </div>
-        <button type="submit" className="bg-black text-white hover:bg-[#EFEFEF] hover:text-neutral-900 transition duration-300 ease-out flex justify-center w-full py-2 px-4 rounded font-bold text-lg">
-            PLACE ORDER
-        </button>
-        </form>
+        )}
+        {paymentSection && (
+          <div>
+            <h1>THIS IS THE PAYMENT SECTION</h1>
+          </div>
+        )}
+      </div>
         );
+    }
+       
 };
